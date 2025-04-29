f437589c0e6aa437bebe8345dd172103497f3d34
daniela-porras <porrasdaniela523@gmail.com>
2025-04-28 21:59:41 -0600

    Se esta haciendo ganadero

--- ProyectoProgramadoI/src/proyectoprogramadoi/controllers/GanaderoController.java
+++ ProyectoProgramadoI/src/proyectoprogramadoi/controllers/GanaderoController.java
@@ -4,10 +4,37 @@
  */
 package proyectoprogramadoi.controllers;
 
+import proyectoprogramadoi.models.Ganadero;
+import proyectoprogramadoi.containers.GanaderoContainer;
+import java.util.List;
+
 /**
  *
  * @author usuario1
  */
-public class GanaderoController {
+public class GanaderoController implements GanaderoInterface{
+
+    @Override
+    public String store(String[] data) {
+        if(GanaderoContainer.exist(data[0])){
+            return "error:Ya existe el ganadero registrado!";
+        }
+        Ganadero obj=new Ganadero();
+        boolean done=GanaderoContainer.store(obj);
+        if(done)  return "done:Ganadero guardado exitosamente!";
+        return "error:Error al guardar los datos";
+    }
+
+    @Override
+    public String[][] getAll() {
+        List ganaderos=GanaderoContainer.getAll();        
+        String[][] data=new String[ganaderos.size()][2];
+        for(int i=0;i<ganaderos.size();i++){
+            Ganadero g=(Ganadero) ganaderos.get(i);
+            data[i][0]=g.getCedula();
+            data[i][1]=g.getNombre();
+        }
+        return data;
+    }
     
 }

6297641ba4d9edb4b201aaee6c5b96040df11185
daniela-porras <porrasdaniela523@gmail.com>
2025-04-29 20:07:29 -0600

    Agregar cambios unversionados

---
+++ ProyectoProgramadoI/src/proyectoprogramadoi/controllers/GanadoInterface.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package proyectoprogramadoi.controllers;
+
+/**
+ *
+ * @author usuario1
+ */
+public interface GanadoInterface {
+    
+    public String store(String data[]);
+    public String[][] getAll();
+    
+}
